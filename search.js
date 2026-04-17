d54874ecda738e1e8e45c67311ae05c97fcf28b1
outlierarise <jrtaylor2024@hotmail.com>
2026-04-17 16:33:42 +1000

    Update search.js

--- search.js
+++ search.js
@@ -4,7 +4,7 @@
 //  Place this file in your asmr-app/ folder
 // ============================================================
 
-const API_KEY = 'AIzaSyAoQMvf9zcG4BgbTNtVnygerxbVC0-ec-4'; // 🔑 Paste your API key here
+const API_KEY = 'AIzaSyDRb3shAfqI5G5RLEXyVead9nLpTDrWdv8'; // 🔑 Paste your API key here
 
 // ============================================================
 //  CATEGORY SEARCH TERMS
